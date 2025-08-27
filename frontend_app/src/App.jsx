24b904d71428a75b82accd89bf0b08657601d69e
dbailey30 <bailey0302@msn.com>
2025-08-27 17:45:49 -0400

    Update App.jsx

--- frontend_app/src/App.jsx
+++ frontend_app/src/App.jsx
@@ -9,7 +9,7 @@ import SubscriptionPlans from './components/SubscriptionPlans'
 import SubscriptionStatus from './components/SubscriptionStatus'
 import BusinessAnalytics from './components/BusinessAnalytics'
 import AddBusiness from './components/AddBusiness'
-import analytics from './services/analytics'
+import { analyticsAPI } from './services/api'
 import './App.css'
 
 function App() {
